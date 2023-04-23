1030e1fe253ce7d7c72e108569b783f7105b4973
sahilhedau49 <hedausahil49@gmail.com>
2023-04-23 15:25:10 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 var input_str = document.querySelector("#input-str");
 let result = document.getElementById("result");
+let btn = document.getElementById("btn");
+
+input_str.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    btn.click();
+  }
+});
 
 checkStr = () => {
   let str = input_str.value.toLowerCase();
-  if (isPalindrome(str)) {
+  if (str.length == 0) {
+    result.innerText = `Please enter valid string.`;
+  } else if (isPalindrome(str)) {
     result.innerText = `Yes! ${input_str.value} is Palindrome. ✔️`;
+    result.style.background = "#7cfd5c";
   } else {
     result.innerText = `No! ${input_str.value} is not Palindrome. ❌`;
+    result.style.background = "#ffa9a9";
   }
 };
 
